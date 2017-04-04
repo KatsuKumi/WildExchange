2fa7dba6bf65ebbcdd8c6badc4365f566f8b5ece
Alex-san <bobbax4@live.fr>
2017-04-04 13:44:39 +0200

    Ajout d'un routing, initialisation de la page base.html.twig

--- src/WCS/WildExchangeBundle/Controller/DefaultController.php
+++ src/WCS/WildExchangeBundle/Controller/DefaultController.php
@@ -10,4 +10,8 @@ class DefaultController extends Controller
     {
         return $this->render('WCSWildExchangeBundle:Default:index.html.twig');
     }
+    public function testAction()
+    {
+        return $this->render('WCSWildExchangeBundle:Default:test.html.twig');
+    }
 }
