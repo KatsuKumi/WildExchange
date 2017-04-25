376fc9e418000a8b9285f0adf7519b6aa06760e2
Lucilediague <lucile.diague0@orange.fr>
2017-04-25 15:35:03 +0200

    ajout docs

---
+++ src/WCS/WildExchangeBundle/Controller/DocsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace WCS\WildExchangeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class DocsController extends Controller
+{
+    public function docsAction(Request $request)
+    {
+        return $this->render('WCSWildExchangeBundle:Default:index.html.twig');
+    }
+}
