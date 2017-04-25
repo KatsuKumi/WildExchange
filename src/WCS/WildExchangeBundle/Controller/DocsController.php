b2679fea3d379953b1620d0542ce0d5ac9fd90d3
Lucilediague <lucile.diague0@orange.fr>
2017-04-25 17:53:00 +0200

    ajout docs officielles et  creation relations entités

--- src/WCS/WildExchangeBundle/Controller/DocsController.php
+++ src/WCS/WildExchangeBundle/Controller/DocsController.php
@@ -8,8 +8,19 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DocsController extends Controller
 {
-    public function docsAction()
+    public function docsAction($tag)
     {
-        return $this->render('WCSWildExchangeBundle:Default:docs.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $tagobj = $em
+            ->getRepository('WCSWildExchangeBundle:Tags')
+            ->findOneByNom($tag);
+        if (empty($tagobj)) {
+            $this->addFlash('connexion', "Ce tag n'existe pas !");
+            return $this->redirectToRoute('tagspage');
+        }
+        return $this->render('WCSWildExchangeBundle:Default:docs.html.twig', array(
+        'tags' => $tagobj
+    ));
     }
 }
