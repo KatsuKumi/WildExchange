985b44512d45bb2b954b130595894bcac69022ef
Lucilediague <lucile.diague0@orange.fr>
2017-04-26 10:59:25 +0200

    ajout de la page ajouter docs fonctionelle avec son bouton dans documentation

--- src/WCS/WildExchangeBundle/Controller/DocsController.php
+++ src/WCS/WildExchangeBundle/Controller/DocsController.php
@@ -4,6 +4,8 @@ namespace WCS\WildExchangeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use WCS\WildExchangeBundle\Entity\Docs;
+use WCS\WildExchangeBundle\Form\DocsType;
 
 
 class DocsController extends Controller
@@ -22,5 +24,48 @@ class DocsController extends Controller
         return $this->render('WCSWildExchangeBundle:Default:docs.html.twig', array(
         'tags' => $tagobj
     ));
+    }
+
+    public function ajoutdocsAction(Request $request, $tag)
+    {
+        if (!$this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $usr= $this->get('security.context')->getToken()->getUser();
+            $this->addFlash('connexion', "Vous devez être connecté pour ajouter une documentation !");
+            return $this->redirectToRoute('connectionpage');
+        }
+        // 1) build the form
+        $doc = new Docs();
+        $form = $this->createForm(DocsType::class, $doc);
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $doc->setDate(new \DateTime());
+            $usr= $this->get('security.context')->getToken()->getUser();
+            $doc->setCreateur($usr);
+            $em = $this->getDoctrine()->getManager();
+            $tagobj = $em
+                ->getRepository('WCSWildExchangeBundle:Tags')
+                ->findOneByNom($tag);
+            $doc->addTag($tagobj);
+            $status = $em
+                ->getRepository('WCSWildExchangeBundle:Status')
+                ->find(1);
+            $doc->setStatus($status);
+            $em->persist($doc);
+            $em->flush();
+            $this->addFlash(
+                'ajoutsuccess',
+                'Votre documentation a bien été ajoutée !'
+            );
+            $this->checkBadges();
+
+            return $this->redirectToRoute('docspage', array('tag'=>$tag, 'page'=>    1));
+        }
+
+        return $this->render(
+            'WCSWildExchangeBundle:Default:ajoutdocs.html.twig',
+            array('form' => $form->createView())
+        );
     }
 }
---
+++ src/WCS/WildExchangeBundle/Form/DocsType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace WCS\WildExchangeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class DocsType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('titre', TextType::class,array(
+            'attr' => array(
+                'placeholder' => 'Titre'
+            )))->add('url', TextType::class,array(
+            'attr' => array(
+                'placeholder' => 'URL'
+            )));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'WCS\WildExchangeBundle\Entity\Docs'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'wcs_wildexchangebundle_docs';
+    }
+
+
+}
